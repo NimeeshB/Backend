ec51fe45a598cb45166a5d621b01dd31c8f19867
Nimeesh <bagwe.nimeesh4@gmail.com>
2024-08-14 11:17:10 -0400

    Util code changes

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser" // server se user ka browser hai uski cookies access aur set karne use hota hai 
+
+const app = express()
+//app banne ke baad hi sab cors and cookie parser configure hota hai 
+
+//some essential middleware in an Express.js application
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, //kaun kaunse frontend se request allow karni hai 
+    credentials: true  // Allows cookies and other credentials to be included in requests
+}))// app.use() sare middleware ya configurations ke liye use hota hai. cors is a middleware. bichme checking hota hai to see if the url is whitelisted 
+
+
+app.use(express.json({limit: "16kb" })) //This middleware is used to parse incoming JSON payloads from HTTP requests and make them accessible via req.body
+app.use(express.urlencoded({extended: true, limit: "16kb"})) //This middleware is used to parse incoming requests with URL-encoded payloads (typically from HTML form submissions) and make them accessible via req.body.
+app.use(express.static("public")) //This middleware serves static files (like images, CSS, JavaScript files, etc.) from the public directory.
+//  How it works: When a request is made for a static asset, Express will look in the public directory to find and serve the file. For example, if you have a file public/styles.css, it would be accessible via http://yourdomain.com/styles.css.
+
+app.use(cookieParser())
+export default {app}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 import { DB_NAME } from "./constants.js";
 import dotenv from "dotenv"
+import app from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => { //part is good for catching server-level errors after the server has started 
+        console.log('Err', error)
+    })
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}` );
+    }) //If the database connection is successful, the server starts listening on a port.
+    //The app.listen() function in an Express.js application is used to start a server and have it listen for incoming requests on a specified port. Essentially, it tells your Node.js application to start accepting HTTP requests at a specific port and IP address.
+}) 
+.catch((error)=> {
+    console.error("MONGODB connection failed!! ", error)
+})
