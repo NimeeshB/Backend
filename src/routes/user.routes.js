e72142c38b35d6079b7f729f08203a25b7c61689
Nimeesh <bagwe.nimeesh4@gmail.com>
2025-05-22 20:43:07 -0400

    Added new routes for User controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,62 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    //upload aaya hia multer se, it is a middleware 
-    upload.fields([{
-        name: "avatar", //ye fields frontend me bhi same name se rehni chahiye 
-        maxCount: 1
-    },{
-        name: "coverImage", //ye fields frontend me bhi same name se rehni chahiye ,
-        maxCount: 1
-    }]),
-    registerUser)// http://localhost:8000/users/register par request anepe registerUser method execute hogi
-    //register method execute hone se pehele hame avatar, coverimage save karna hai local storage me 
+  //upload aaya hia multer se, it is a middleware
+  upload.fields([
+    {
+      name: "avatar", //ye fields frontend me bhi same name se rehni chahiye
+      maxCount: 1,
+    },
+    {
+      name: "coverImage", //ye fields frontend me bhi same name se rehni chahiye ,
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+); // http://localhost:8000/users/register par request anepe registerUser method execute hogi
+//register method execute hone se pehele hame avatar, coverimage save karna hai local storage me
 
-    router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-//secured routes: ye routes hit karne ke liye user logged in hona chaiye 
-router.route("/logout").post(verifyJWT,  logoutUser) //verifyJWT middleware function hai jo logout function execute honese pehele execute hoga which will give us the user information which will eventually be used to logout the use  
+//secured routes: ye routes hit karne ke liye user logged in hona chaiye
+router.route("/logout").post(verifyJWT, logoutUser); //verifyJWT middleware function hai jo logout function execute honese pehele execute hoga which will give us the user information which will eventually be used to logout the use
 
-//user ka accesstoken expire hone ke baad usko naya accesstoken deneka endpoint 
-router.route("/refreshToken").post( refreshAccessToken )
+//user ka accesstoken expire hone ke baad usko naya accesstoken deneka endpoint
+router.route("/refreshToken").post(refreshAccessToken);
 
-export default router
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //username params se milega thats why such route
+
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+export default router;
